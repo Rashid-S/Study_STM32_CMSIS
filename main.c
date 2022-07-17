36869aaadd4f95c6a796d3d2d05a2b7b1f2540ca
Rashid-S <Rashid-S>
2022-07-17 14:48:54 +0300

    STM32L476. Прерывание на таймере TIM3

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "stm32l4xx.h"
 
-//#define SYSLOCK    80000000U                 // макрос максимальной системной частоты
+//#define SYSLOCK    80000000U  // макрос максимальной системной частоты при тактировании от HSI
 
 #define TIM_EnableIT_UPDATE(TIMx) TIMx->DIER |= TIM_DIER_UIE  // Макрос разрешения прерывания
 #define TIM_EnableCounter(TIMx)   TIMx->CR1 |= TIM_CR1_CEN     // Макрос включения таймера
@@ -9,68 +9,20 @@
 #define PIN_B5_OFF()  GPIOB->BSRR |=GPIO_BSRR_BR5;       // выключить пин B5
 
 
+//----- Инициализация таймера TIM3 -------------
+// @note - Тактирование производится от MSI с частотой 4МГц
 static void TIM3_Init (void)
 	{
-		RCC->APB1ENR1  |=  RCC_APB1ENR1_TIM3EN;
-		NVIC_EnableIRQ (TIM3_IRQn);
-		WRITE_REG(TIM3->PSC, 7999);                          // задаем количество тактов за 1 секунду
-		WRITE_REG(TIM1->ARR, 10000);                         // 
+		RCC->APB1ENR1  |=  RCC_APB1ENR1_TIM3EN;       // включаем тактирование таймера от шины APB1
+		NVIC_EnableIRQ (TIM3_IRQn);                   // разрешаем глобальное прерывание для таймера
+		WRITE_REG(TIM3->PSC, 7999);                   // задаем делителем какое количество тактов должно быть за 1 секунду
+		WRITE_REG(TIM3->ARR, 500);                  // задаем длительность в 1 секунду
 	}
 
 
-
-
 __IO uint32_t SysTick_CNT = 0;
+__IO uint8_t tim3_cnt	    = 0;
 
-
-////////void SetSysClockTo80(void)
-////////{
-////////	
-////////	/* Enable Prefetch Buffer  */
-////////	FLASH->ACR &= ~FLASH_ACR_LATENCY;
-////////	FLASH->ACR |= FLASH_ACR_LATENCY_4WS;	
-////////	
-////////	/* Enable HSI */
-////////	RCC->CR |= RCC_CR_HSION;
-////////	
-////////	/* wait till HSI is ready  */	
-////////	//while(READ_BIT(RCC->CR, RCC_CR_HSIRDY == RESET));
-////////	while(!(RCC->CR & RCC_CR_HSIRDY));
-////////	
-////////	/* Configure AHB PRESCALARS HCLK  */
-////////	RCC->CFGR  &= ~RCC_CFGR_HPRE;
-////////	RCC->CFGR  |=  RCC_CFGR_HPRE_DIV1;
-////////	
-////////	/* Configure AHB1 PRESCALARS */
-////////	RCC->CFGR  |=  RCC_CFGR_PPRE1_DIV1;
-////////	
-////////	/* Configure AHB2 PRESCALARS */
-////////	RCC->CFGR  |=  RCC_CFGR_PPRE2_DIV1;
-////////	
-////////	/* Configure MAIN PLL  */
-////////	//RCC->CR &= RCC_CR_PLLON;
-////////	RCC->PLLCFGR &=  (RCC_PLLCFGR_PLLM);
-////////	
-////////	RCC->PLLCFGR &=  ~RCC_PLLCFGR_PLLN;
-////////	RCC->PLLCFGR |=  (RCC_PLLCFGR_PLLN_1 | RCC_PLLCFGR_PLLN_3);
-////////	
-////////	RCC->PLLCFGR |=  RCC_PLLCFGR_PLLSRC_HSI;
-////////	RCC->PLLCFGR &=  ~RCC_PLLCFGR_PLLR;	
-////////	
-////////	/* Enable PLLCLK  */
-////////	RCC->PLLCFGR  |=  RCC_PLLCFGR_PLLREN;
-////////	
-////////	
-////////	/* Enable the PLL and for it to become ready  */
-////////	RCC->CR |=  RCC_CR_PLLON;
-////////	while (!(RCC->CR & RCC_CR_PLLRDY));
-////////	
-////////	/* Select the Clock Source and wait for it to be set  */
-////////	RCC->CFGR  |=  RCC_CFGR_SW_PLL;
-////////	
-////////	/* Wait till PLL is used as clock source  */
-////////		while(!(RCC->CFGR & RCC_CFGR_SWS));
-////////}
 
 
 void SysTick_Init(void)
@@ -86,15 +38,6 @@ void SysTick_Handler(void)
 	if (SysTick_CNT > 0) SysTick_CNT--;
 	
 }
-
-void delay_ms (uint32_t ms)
-{
-	SysTick->VAL &= ~SysTick_VAL_CURRENT_Msk;
-	SysTick->VAL = 80000000/1000-1;
-	SysTick_CNT = ms;
-	while(SysTick_CNT);
-	
-}	
 
 
 void PORT_5_INIT (void)
@@ -130,12 +73,24 @@ int main (void)
 	}
 }
 
-
+// ---------- Обработчик прерывания ---------------
 void TIM3_IRQHandler(void)
 	{
 		if (READ_BIT(TIM3->SR, TIM_SR_UIF))
 		{
+			CLEAR_BIT(TIM3->SR, TIM_SR_UIF);
 			
+			switch(tim3_cnt)
+			{
+				case 0: PIN_B5_ON(); break;
+				case 1: PIN_B5_OFF(); break;
+			}
+			
+			tim3_cnt++;
+			if (tim3_cnt > 5) 
+			{
+				tim3_cnt = 0;
+			}
 		}
 	}
 
